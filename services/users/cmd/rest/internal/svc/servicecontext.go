0ebb0bcac7b05a84ce0c85a865587c4d6e903c38
werido <359066432@qq.com>
2024-03-29 14:02:47 +0800

    feat:update data init

--- services/users/cmd/rest/internal/svc/servicecontext.go
+++ services/users/cmd/rest/internal/svc/servicecontext.go
@@ -63,15 +63,26 @@ func LoadInitData(c config.Config, DB *gorm.DB) {
 			fmt.Println("add all resource permission to role_id: ", permission["role_id"])
 			// 先删除role_id已经有的权限
 			DB.Unscoped().Where("role_id = ?", permission["role_id"]).Delete(&models.ResourcePermission{})
-			// DB.Commit()
-			var count int64
-			DB.Model(&models.Resource{}).Count(&count)
-			var resourceList []int
-			for i := 1; i <= int(count); i++ {
-				resourceList = append(resourceList, i)
+			// 查询当前所有的resource_id
+			resources, err := models.QueryResource(map[string]interface{}{}, DB)
+			if err != nil {
+				fmt.Println("查询所有资源失败：", err)
 			}
-			err := models.BatchBindResourcePermission(resourceList, permission["role_id"].(int), DB)
-			fmt.Println("err:", err)
+			var resourceIdList []int
+			for _, v := range resources {
+				resourceIdList = append(resourceIdList, v.ID)
+			}
+
+			// var count int64
+			// DB.Model(&models.Resource{}).Count(&count)
+			// // var resourceList []int
+			// for i := 1; i <= int(count); i++ {
+			// 	resourceList = append(resourceList, i)
+			// }
+			err = models.BatchBindResourcePermission(resourceIdList, permission["role_id"].(int), DB)
+			if err != nil {
+				fmt.Println("fail to bind resource to role -> ", err)
+			}
 		} else {
 			var list []models.ResourcePermission
 			// 先删除role_id已经有的权限
